1afdb6edf73c19db32142d23780b148e6e297e50
Enna Zhou <enna_zhou@mymail.sutd.edu.sg>
2019-04-09 23:48:02 +0800

    map
    mapUI

---
+++ MMO_game/Assets/Script/Scene Management/Map.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    public GameObject mapUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mapUI.SetActive(false);
+    }
+
+
+}
