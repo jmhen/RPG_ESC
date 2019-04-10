11c0e95bfc86d48327eb3fff05de65f7936a9ef8
Enna Zhou <enna_zhou@mymail.sutd.edu.sg>
2019-04-10 13:30:46 +0800

    some minor change

--- MMO_game/Assets/Script/Scene Management/MainMenu.cs
+++ MMO_game/Assets/Script/Scene Management/MainMenu.cs
@@ -6,16 +6,19 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     static MainMenu mainMenu;
+
     // Start is called before the first frame update
     private void Awake()
     {
         mainMenu = this;
     }
+
     public void PlayGame()
     {
-        SceneManager.LoadScene("GameElements");
 
-        SceneManager.LoadScene(1,LoadSceneMode.Additive);
+        SceneManager.LoadScene("GameElements");
+        SceneManager.LoadScene(1, LoadSceneMode.Additive);
+
     }
 
     // Update is called once per frame
