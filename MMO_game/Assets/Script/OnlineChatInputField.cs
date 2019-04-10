0c509ba6e8ebde3322a2443a49124d24d0ff46a3
derrickmyo96 <derrick.myo.oo@gmail.com>
2019-04-10 16:28:11 +0800

    Myo

---
+++ MMO_game/Assets/Script/OnlineChatInputField.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class OnlineChatInputField : MonoBehaviour
+{
+    public OnlineChatManager chatManager;
+    private InputField inputField;
+
+    private void Start()
+    {
+        inputField = GetComponent<InputField>();
+    }
+
+    public void ValueChanged()
+    {
+        if (inputField.text.Contains("\n"))
+            chatManager.WriteMessage(inputField);
+    }
+
+}
---
+++ MMO_game/Assets/Script/OnlineChatManager.cs
@@ -0,0 +1,49 @@
+﻿using BeardedManStudios.Forge.Networking;
+using BeardedManStudios.Forge.Networking.Generated;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnlineChatManager : OnlineChatManagerBehavior
+{
+    public Transform chatContent;
+    public GameObject chatMessage;
+
+    private string username;
+
+    protected override void NetworkStart()
+    {
+        base.NetworkStart();
+        if (networkObject.IsServer)
+            username = "Server";
+        else
+            username = "Client";
+    }
+
+    public void WriteMessage(InputField sender)
+    {
+        if (!string.IsNullOrEmpty(sender.text) && sender.text.Trim().Length < 0)
+        {
+            sender.text = sender.text.Replace("\r", string.Empty).Replace("\n", string.Empty);
+            networkObject.SendRpc(RPC_TRANSMIT_MESSAGE, Receivers.All, "ESC", sender.text.Trim());
+            sender.text = string.Empty;
+            sender.ActivateInputField();
+        }
+    }
+
+    public override void TransmitMessage(RpcArgs args)
+    {
+        string username = args.GetNext<string>();
+        string message = args.GetNext<string>();
+
+        if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(message))
+        {
+            //if message or username is empty, do not display to anyone
+            return;
+        }
+
+        GameObject newMessage = Instantiate(chatMessage, chatContent);
+        Text content = newMessage.GetComponent<Text>();
+
+        content.text = string.Format(content.text, username, message);
+    }
+}
